2ef6c0d1890dbab58b2da320a0b12e7b71d19a31
Saroj Kumar <ssaroj1718@gmail.com>
2020-08-02 15:53:16 +0530

    final fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,3 +9,18 @@
 // 		createPage(page)
 // 	}
 // }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+	if (stage === "build-html") {
+		actions.setWebpackConfig({
+			module: {
+				rules: [
+					{
+						test: /materialize-css/,
+						use: loaders.null(),
+					},
+				],
+			},
+		})
+	}
+}
